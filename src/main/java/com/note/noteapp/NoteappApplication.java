27791df588d4aa8a1ecf09ed1e517688ebb772eb
Teriyaki229 <sudipta.gogoi3536@gmail.com>
2023-08-10 00:54:59 +0530

    Basic CRUD operations

---
+++ src/main/java/com/note/noteapp/NoteappApplication.java
@@ -0,0 +1,13 @@
+package com.note.noteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/note/noteapp/NoteappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note.noteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
